4a3c9c9345895befbc738a32c9bd91e446678126
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2022-06-22 21:38:36 +0800

    first

--- Sources/ShamePackage/ShamePackage.swift
+++ Sources/ShamePackage/ShamePackage.swift
@@ -1,9 +1,9 @@
 import CoreML
 import NaturalLanguage
 
-@available(iOS 13.0, *)
-struct ShemaPackage {
-    static let shared = Self()
+@available(iOS 13.0, macOS 10.15, *)
+public struct ShamePackage {
+    public static let shared = Self()
 
     let sb = {
         try! NLModel(mlModel: SbOnYou_4(configuration: .init()).model)
@@ -15,15 +15,15 @@ struct ShemaPackage {
         try! NLModel(mlModel: TeaOnYou_1(configuration: .init()).model)
     }()
 
-    func sb(_ text: String) -> Bool {
+    public func sb(_ text: String) -> Bool {
         sb.predictedLabel(for: text) == "sb"
     }
 
-    func shame(_ text: String) -> Bool {
+    public func shame(_ text: String) -> Bool {
         shame.predictedLabel(for: text) == "shame"
     }
 
-    func tea(_ text: String) -> Bool {
+    public func tea(_ text: String) -> Bool {
         tea.predictedLabel(for: text) == "tea"
     }
 }
--- Tests/ShamePackageTests/ShamePackageTests.swift
+++ Tests/ShamePackageTests/ShamePackageTests.swift
@@ -2,5 +2,7 @@ import XCTest
 @testable import ShamePackage
 
 final class ShamePackageTests: XCTestCase {
-    
+    func testExample() throws {
+        XCTAssertTrue(ShamePackage.shared.shame("大哥，求你哄哄她吧，她心情好了，就愿意理我了"))
+    }
 }
